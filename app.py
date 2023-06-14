dc1d8f6b21add243a23c8835c277423388c38301
David Huang <david.zq.huang@gmail.com>
2023-06-14 16:33:21 -0400

    updated

--- app.py
+++ app.py
@@ -6,21 +6,45 @@ import os
 import pypdfium2 as pdfium
 
 @olo.register()
-def pdfimageiter(file = olo.File(), func = olo.Func()):
-    # from pdf2image import convert_from_path
-    # images = convert_from_path(file)
-    # page_outputs = []
-    # for i, image in enumerate(images):
-    #     print(f"Processing page {i} ", image)
-    #     with open("page.jpg", "wb") as f:
-    #         image.save(f, "JPEG")
-    #         
-    #         file_json = olo.upload_file("page.jpg")
-    #         page_output = func(file_json[0])
-    #         page_outputs.append(page_output)
-    # return page_outputs
+def get_num_pages(file = olo.File()):
+    pdf = pdfium.PdfDocument(file)
+    print("PDF", len(pdf))
+    return len(pdf)
+
+@olo.register()
+def pdfpage2image(file = olo.File(), num = olo.Num()):
+    pdf = pdfium.PdfDocument(file)
+    page = pdf[num]
+    bitmap = page.render(
+        scale=1,
+    )
+    pil_image = bitmap.to_pil()
+    pil_image.save("page.jpg", "JPEG")
+    return olo.OutputFile("page.jpg")
+
+@olo.register()
+def pdfpageannotation2image(file = olo.File(), num = olo.Num(), annotation = olo.Json()):
+    # {"x1":64,"x2":308,"y1":615,"y2":728,"label":"0","confidence":0.9146367907524109}
+    pdf = pdfium.PdfDocument(file)
+    page = pdf[num]
+    bitmap = page.render(
+        scale=1,
+    )
+    pil_image = bitmap.to_pil()
     
-    # use pdfium
+    # crop
+    x1 = annotation["x1"]
+    x2 = annotation["x2"]
+    y1 = annotation["y1"]
+    y2 = annotation["y2"]
+    pil_image = pil_image.crop((x1, y1, x2, y2))
+    
+    pil_image.save("page.jpg", "JPEG")
+    
+    return olo.OutputFile("page.jpg")
+
+@olo.register()
+def pdfimageiter(file = olo.File(), func = olo.Func()):
     pdf = pdfium.PdfDocument(file)
     page_outputs = []
     for i in range(len(pdf)):
@@ -46,6 +70,13 @@ def pdf2txt(file = olo.File()):
         from PyPDF2 import PdfReader
         reader = PdfReader(file)
         return "\n".join([page.extract_text() for i, page in enumerate(reader.pages)])
+    
+@olo.register()
+def pdfpage2txt(file = olo.File(), num = olo.Num()):
+    with open(file, "rb") as file:
+        from PyPDF2 import PdfReader
+        reader = PdfReader(file)
+        return reader.pages[num].extract_text()
 
 @olo.register(description="Extracts text from PDF, paginated")
 def pdf2txt_paginated(file = olo.File()):
